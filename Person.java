bb7b79d93d175e58575f7f97f4aa809d4eaeb226
Armanda Barus <armanda.barus@students.amikom.ac.id>
2018-12-03 16:34:56 +0700

    penambahan file Hello.java dan Person.java

---
+++ Person.java
@@ -0,0 +1,11 @@
+package bank.customers;
+
+public class Person {
+
+	String nama;
+	String noKtp;
+	@Override
+	public String toString() {
+		return "Person [nama=" + nama + ", noKtp=" + noKtp + "]";
+	}
+}
---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello kagura, selamat sore udah malem ini!");
+	
+	}
+	
+}
