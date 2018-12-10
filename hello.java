cfd8faf202ef960d9d45b632aaa488fd325ade80
jordi <muhammad.jordi@students.amikom.ac.id>
2018-12-10 16:19:32 +0700

    sukses

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 	System.out.println("Hello kagura, selamat sore udah malem ini!");
-	
+	System.out.println("Selamat sore armanda?");
 	System.out.println("Sudah makan belum?");
 	
 	
